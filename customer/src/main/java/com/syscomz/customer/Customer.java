b28d0be1c0c9fd6dbafdc574b0a49c6be53e9ea1
Borislav Dostumski <b.dostumski@gmail.com>
2023-02-26 14:02:42 +0200

    code style

--- customer/src/main/java/com/syscomz/customer/Customer.java
+++ customer/src/main/java/com/syscomz/customer/Customer.java
@@ -7,24 +7,24 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-    @Data
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Entity
-    public class Customer {
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Customer {
 
-        @Id
-        @SequenceGenerator(
-                name = "customer_id_sequence",
-                sequenceName = "customer_id_sequence"
-        )
-        @GeneratedValue(
-                strategy = GenerationType.SEQUENCE,
-                generator = "customer_id_sequence"
-        )
-        private Integer id;
-        private String firstName;
-        private String lastName;
-        private String email;
-    }
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
